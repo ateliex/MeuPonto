fdab4d3411abe72015eb97a3390106347c837983
Marcelo Dias <mardsystems@gmail.com>
2024-12-08 12:44:52 -0300

    Include gitignore rule

---
+++ src/MeuPonto.DomainModel/Features/BackupComprovantes/BackupComprovantesFacade.cs
@@ -0,0 +1,24 @@
+﻿using System.Transactions;
+using MeuPonto.Models.Pontos;
+
+namespace MeuPonto.Features.BackupComprovantes;
+
+public static class BackupComprovantesFacade
+{
+    public static Comprovante CriaComprovante(TransactionContext transaction, Guid? id = null)
+    {
+        var comprovante = new Comprovante
+        {
+            Id = id ?? Guid.NewGuid(),
+            CreationDate = transaction.DateTime
+        };
+
+        return comprovante;
+    }
+
+    public static void RecontextualizaComprovante(this Comprovante comprovante, TransactionContext transaction, Guid? id = null)
+    {
+        comprovante.Id = comprovante.Id ?? id ?? Guid.NewGuid();
+        comprovante.CreationDate = transaction.DateTime;
+    }
+}
